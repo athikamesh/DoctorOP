262d1cc5ff4c102a43e6be36234e6aae0ebff604
Athi Kamesh <athikamesh1986@outlook.com>
2021-09-17 17:52:29 +0530

    Dashboard Work Updated

---
+++ DoctorOP/AddMedicin.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoctorOP
+{
+    /// <summary>
+    /// Interaction logic for AddMedicin.xaml
+    /// </summary>
+    public partial class AddMedicin : Window
+    {
+        DOPEntities dOPEntities = new DOPEntities();
+        public AddMedicin()
+        {
+            InitializeComponent();
+            txt_medid.Text = GetPateintID().ToString();
+        }
+        int GetPateintID()
+        {
+            int ID = 1000;
+            var pid = dOPEntities.Medicin_tbl.OrderByDescending(id => id.Id).ToList();
+            if (pid.Count > 0)
+            {
+                ID = int.Parse(pid[0].Med_Id) + 1;
+            }
+            return ID;
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(txt_medid.Text!="" && txt_medname.Text!="" && txt_medprice.Text!="" && txt_medqty.Text!="" && cmb_medtype.Text!="")
+                {
+                    Medicin_tbl medicin_Tbl = new Medicin_tbl();
+                    medicin_Tbl.Med_Id = txt_medid.Text;
+                    medicin_Tbl.Med_Name = txt_medname.Text;
+                    medicin_Tbl.Med_Price = txt_medprice.Text;
+                    medicin_Tbl.Med_Qty = txt_medqty.Text;
+                    medicin_Tbl.Med_type = cmb_medtype.Text;
+                    dOPEntities.Medicin_tbl.Add(medicin_Tbl);
+                    int i=dOPEntities.SaveChanges();
+                    if (i > 0) { MessageBox.Show("Added Successfully"); } else { MessageBox.Show("Not Added Successfully"); }
+                }
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+              txt_medid.Text="";
+              txt_medname.Text="";
+              txt_medprice.Text="";
+              txt_medqty.Text="";
+              cmb_medtype.Text="";
+              txt_medid.Text = GetPateintID().ToString();
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
--- DoctorOP/MainWindow.xaml.cs
+++ DoctorOP/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,15 +23,74 @@ namespace DoctorOP
     /// </summary>
     public partial class MainWindow : Window
     {
+        DOPEntities dOPEntities = new DOPEntities();
         public MainWindow()
         {
             InitializeComponent();
+            LoadPieChart(); LoadColumnChart();
+            PointLabel = chartPoint =>
+               string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+            DataContext = this;
         }
+        public Func<ChartPoint, string> PointLabel { get; set; }
 
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 4;
+        }
         private void Btn_consulting_Click(object sender, RoutedEventArgs e)
         {
             PatientConsulting PC = new PatientConsulting();
             PC.ShowDialog();
         }
+
+        void LoadPieChart()
+        {
+            var data = dOPEntities.Patientvisit.GroupBy(b=>b.patient_Complaint).ToList();
+            foreach(var dat in data)
+            {               
+                PieSeries pieSeries = new PieSeries();
+                pieSeries.Title = dat.Key.ToString();
+                pieSeries.Values = new ChartValues<ObservableValue> { new ObservableValue(dat.Count()) };
+                pieSeries.ToolTip = "New Idea1" + 20;
+                pieSeries.DataLabels = true;
+                anl_chart.Series.Add(pieSeries);
+            }
+
+            
+        }
+        void LoadColumnChart()
+        {
+            var data = dOPEntities.Payment_tbl.GroupBy(b => b.payment_date).ToList();
+
+            foreach (var dat in data)
+            {
+                
+                SeriesCollection = new SeriesCollection();
+                ColumnSeries columnSeries = new ColumnSeries();
+                columnSeries.Title = "";
+                columnSeries.Values = new ChartValues<ObservableValue> { new ObservableValue(10) };
+                columnSeries.DataLabels = true;
+                SeriesCollection.Add(columnSeries);
+                Labels = new [] { dat.Key.ToString()};
+            }
+            Formatter = value => value.ToString("N");
+
+        }
+        private void btn_medicin_Click(object sender, RoutedEventArgs e)
+        {
+            AddMedicin AMed = new AddMedicin();
+            AMed.ShowDialog();
+        }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
     }
 }
